47cb583ddcc943408532a155d33c3bb6cc8acb2f
mihaiolteanu <mihai_olteanu@fastmail.fm>
2019-06-12 09:33:00 +0300

    Use parameters and constants instead of lexical variables

--- muse-player.lisp
+++ muse-player.lisp
@@ -1,97 +1,107 @@
 (in-package :muse-player)
 
-(let ((still-playing nil)
-      (playing-thread nil)
-      (artist nil)
-      (song nil))
-  
-  (defun set-playing-song (artist-and-song)
-    (setf artist (first artist-and-song))
-    (setf song (second artist-and-song)))
+(defparameter *still-playing* nil
+  "Set to true when player started, set to nil when stopped.")
 
-  (defun what-is-playing ()
-    (list artist song))
+(defparameter *playing-thread* nil
+  "Playing a song or list of songs starts a new thread since we also want to
+  return from the call while the player is running. ")
 
-  (defun start-playing ()
-    (setf still-playing T))
+(defparameter *artist* nil
+  "The name of the currently playing artist.")
 
-  (defun stop ()
-    (when still-playing
-      (setf still-playing nil)
-      (youtube:quit)
-      (join-thread playing-thread)
-      (set-playing-song nil)))
-  
-  (defun play-simple (songs-generator)
-    ;; Quit player and wait for cleanup before starting another play.
-    (stop)
-    (start-playing)
-    (setf playing-thread
-          (make-thread
-           (lambda ()
-             ;; Take a new song from the generator, save the lyrics for it,
-             ;; scrobble it if needed and either find a youtube url to play or
-             ;; create a string that can be searched by youtube-dl. Repeat after
-             ;; the song finishes or was stopped by the user. End the loop only
-             ;; when the player is stopped by the user.
-             (loop for artist-and-song = (next songs-generator)
-                     then (next songs-generator)
-                   ;; Make sure there is a way to stop this endless loop.
-                   while still-playing
-                   do (progn
-                        (set-playing-song artist-and-song)                        
-                        (let ((initial-artist artist)
-                              (initial-song song))
-                          (make-thread
-                           (lambda ()
-                             ;; Save the lyrics for each song that is being played.
-                             (lyrics artist song)
-                             ;; If, after waking up, the same song is playing,
-                             ;; scrobble it.
-                             (sleep 20)
-                             (when (and (string-equal initial-artist artist)
-                                        (string-equal initial-song song))
-                               (track-scrobble artist song
-                                               (timestamp-to-unix (now)))))))
-                        
-                        (youtube:play
-                         (or (song-youtube-url artist song)
-                             (concatenate 'string artist " " song)))
-                        ;; Wait for this song to end before playing the next one.
-                        )))
-           :name "muse playing thread")))
+(defparameter *song* nil
+  "The name of the currently playing song.")
 
-  (defun play-song (artist song)
-    "Play and replay a single song"
-    (play-simple (make-generator ()
-                     (loop do (yield (list artist song))))))
+(defconstant +scrobble-timeout* 30
+  "Time to wait, in seconds, before scrobbling the current playing song.")
 
-  (defun play-artist (artist nsongs random)
-    (play-simple (artist-songs artist nsongs random)))
+(defun set-playing-song (artist-and-song)
+    (setf *artist* (first artist-and-song))
+    (setf *song* (second artist-and-song)))
 
-  (defun play-artist-album (artist album)
-    (play-simple (artist-album-songs artist album)))
+(defun what-is-playing ()
+  (list *artist* *song*))
 
-  (defun play-tag (tag nsongs random)
-    (play-simple (tag-songs tag nsongs random)))
+(defun start-playing ()
+  (setf *still-playing* T))
 
-  (defun play-user-songs (username nsongs random)
-    (play-simple (user-songs username nsongs random)))
+(defun stop ()
+  (when *still-playing*
+    (setf *still-playing* nil)
+    (youtube:quit)
+    (join-thread *playing-thread*)
+    (set-playing-song nil)))
 
-  (defun play-artist-similar-artists (artist nartists nsongs)
-    (play-simple (artist-similar-artists-songs artist nartists nsongs)))
+(defun play-simple (songs-generator)
+  "The main functionality. Take a new song from the generator, save the lyrics
+for it, scrobble it if needed and either find a youtube url to play or create a
+string that can be searched by youtube-dl. Repeat after the song finishes or was
+stopped by the user. End the loop only when the player is stopped by the user."
+  (stop)
+  (start-playing)
+  (setf *playing-thread*
+        (make-thread
+         (lambda ()
+           (loop for artist-and-song = (next songs-generator)
+                   then (next songs-generator)
+                 ;; Make sure there is a way to stop this endless loop.
+                 while *still-playing*
+                 do (progn
+                      (set-playing-song artist-and-song)
+                      (let ((initial-artist *artist*) ;Setup for scrobbling
+                            (initial-song *song*))
+                        (make-thread
+                         (lambda ()
+                           ;; Save the lyrics for each song that is being played.
+                           (lyrics *artist* *song*)
+                           ;; If, after waking up, the same song is playing,
+                           ;; scrobble it.
+                           (sleep +scrobble-timeout+)
+                           (when (and (string-equal initial-artist *artist*)
+                                      (string-equal initial-song *song*))
+                             (track-scrobble *artist* *song*
+                                             (timestamp-to-unix (now)))))))
+                      (youtube:play
+                       (or (song-youtube-url *artist* *song*)
+                           (concatenate 'string *artist* " " *song*)))
+                      ;; Wait for this song to end before playing the next one.
+                      )))
+         :name "muse playing thread")))
 
-  (defun play-tag-similar-artists (tag nartists nsongs)
-    (play-simple (tag-similar-artists-songs tag nartists nsongs)))
+;; Exported interfaces. Most of these functions simply create lastfm generators
+;; that are passed to play-simple.
+(defun play-song (artist song)
+  "Play and replay a single song"
+  (play-simple (make-generator ()
+                 (loop do (yield (list artist song))))))
 
-  (defun song-lyrics ()
-    (lyrics artist song))
+(defun play-artist (artist nsongs random)
+  (play-simple (artist-songs artist nsongs random)))
 
-  (defun love-song ()
-    (track-love artist song))
+(defun play-artist-album (artist album)
+  (play-simple (artist-album-songs artist album)))
 
-  (defun unlove-song ()
-    (track-unlove artist song)))
+(defun play-tag (tag nsongs random)
+  (play-simple (tag-songs tag nsongs random)))
+
+(defun play-user-songs (username nsongs random)
+  (play-simple (user-songs username nsongs random)))
+
+(defun play-artist-similar-artists (artist nartists nsongs)
+  (play-simple (artist-similar-artists-songs artist nartists nsongs)))
+
+(defun play-tag-similar-artists (tag nartists nsongs)
+  (play-simple (tag-similar-artists-songs tag nartists nsongs)))
+
+(defun song-lyrics ()
+  (lyrics *artist* *song*))
+
+(defun love-song ()
+  (track-love *artist* *song*))
+
+(defun unlove-song ()
+  (track-unlove *artist* *song*))
 
 (defun next-song ()
   "Close the current youtube session, forcing the generator to take and play the
